f85f60ae0844d5dc1fedf9b85909df14fee9adfc
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2021-03-23 00:18:57 -0700

    added static files

--- portfolio_app/blog/models.py
+++ portfolio_app/blog/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Blog(models.Model):
-    
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+    #image = models.ImageField(upload_to='portfolio/images/')
+    #url = models.URLField(blank=True)
--- portfolio_app/blog/views.py
+++ portfolio_app/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    blogs = Blog.objects.all().order_by('-date')
+    return render(request, 'blog/all_blogs.html', {'blogs':blogs})
